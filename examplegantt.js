ec7ac6a27c5ca56f9e000468826b790393d44636
Brian Conte <brian@fasttrackteam.com>
2017-10-13 02:22:57 -0700

    Tweaked bar font and tooltip styles

--- examplegantt.js
+++ examplegantt.js
@@ -1,7 +1,7 @@
 var tasks = [
   {"startDate":new Date("Jun 1 1983"),"endDate":new Date("Jan 1 1988"),"taskName":"Microsoft","status":"SUCCEEDED"},
   {"startDate":new Date("Jan 1 1988"),"endDate":new Date("Jan 1 1991"),"taskName":"hDC","status":"SUCCEEDED"},
-  {"startDate":new Date("Jan 1 1991"),"endDate":new Date("Aug 29 1996"),"taskName":"Express Systems","status":"SUCCEEDED"},
+  {"startDate":new Date("Jan 1 1991"),"endDate":new Date("Aug 29 1996"),"taskName":"Express","status":"SUCCEEDED"},
   {"startDate":new Date("Sep 1 1996"),"endDate":new Date("Sep 1 1999"),"taskName":"WRQ","status":"SUCCEEDED"},
   {"startDate":new Date("Sep 1 1999"),"endDate":new Date("Jun 1 2017"),"taskName":"Fast Track","status":"SUCCEEDED"},
   {"startDate":new Date("Jan 1 2005"),"endDate":new Date("Jun 1 2017"),"taskName":"Wild Noodle","status":"SUCCEEDED"},
@@ -14,7 +14,7 @@ var taskStatus = {
     "KILLED" : "bar-killed"
 };
 
-var taskNames = [ "Microsoft", "hDC", "Express Systems", "WRQ", "Fast Track", "Wild Noodle" ];
+var taskNames = [ "Microsoft", "hDC", "Express", "WRQ", "Fast Track", "Wild Noodle" ];
 
 tasks.sort(function(a, b) {
     return a.endDate - b.endDate;
